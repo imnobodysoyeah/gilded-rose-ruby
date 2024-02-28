e8ecd83691b42014aa831426188b30ce7cede0ee
Tori Mayhall <torimayhall@gmail.com>
2024-02-28 02:24:38 -0600

    Removed old case names

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -61,17 +61,4 @@ end
     (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
     new(quality, days_remaining)
   end
-
-  def self.klass_for(name)
-    case name
-    when "Normal Item"
-      Normal
-    when "Aged Brie"
-      Brie
-    when "Sulfuras, Hand of Ragnaros"
-      Item
-    when "Backstage passes to a TAFKAL80ETC concert"
-      Backstage
-    end
-  end
 end
