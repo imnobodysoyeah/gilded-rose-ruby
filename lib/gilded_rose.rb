b890d456db05225a1a55181425dcbe4e0a2bab8f
Tori Mayhall <torimayhall@gmail.com>
2024-02-28 00:52:08 -0600

    Added normal class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,5 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_reader :item, :name, :days_remaining, :quality
 
   def initialize(name:, days_remaining:, quality:)
     @name = name
@@ -19,14 +19,36 @@ class GildedRose
       return backstage_tick
     end
   end
-  
-  def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
 
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+  def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
   end
+  
+  def quality
+    return item.quality if item
+    @quality
+  end
+
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+
+  class Normal
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+
   def brie_tick
     @days_remaining -= 1
     return if @quality >= 50
