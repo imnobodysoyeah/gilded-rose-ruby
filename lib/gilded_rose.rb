b363c98096d9efffa8b3c988e8aa1f65827af420
Tori Mayhall <torimayhall@gmail.com>
2024-02-28 02:32:31 -0600

    Added conjured class and changed spec file to run all tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,12 +9,13 @@ module GildedRose
     end
   end
 
-  class Item
-    attr_reader :quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
+  class Conjured < Item
     def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
     end
   end
   
@@ -54,7 +55,8 @@ end
   SPECIALIZED_CLASSES = {
     'Normal Item' => Normal,
     'Aged Brie' => Brie,
-    'Backstage passes to a TAFKAL80ETC concert' => Backstage
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage,
+    "Conjured Mana Cake" => Conjured
   }
 
   def self.new(name:, quality:, days_remaining:)
