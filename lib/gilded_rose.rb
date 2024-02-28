4ee47b4740fd46a73c5693065dd9e0370c388133
Tori Mayhall <torimayhall@gmail.com>
2024-02-28 02:21:32 -0600

    Added hashes to map case names

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,7 +1,65 @@
 module GildedRose
 
+  class Item
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+    end
+  end
+
+  class Item
+    attr_reader :quality, :days_remaining
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
+    def tick
+    end
+  end
+  
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+  
+      @quality -= 1
+      @quality -= 1 if @days_remaining <= 0
+    end
+  end
+  
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+  
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
+  end
+
+class Backstage < Item
+  def tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+  end
+end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item' => Normal,
+    'Aged Brie' => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage
+  }
+
   def self.new(name:, quality:, days_remaining:)
-    klass_for(name).new(quality, days_remaining)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).
+    new(quality, days_remaining)
   end
 
   def self.klass_for(name)
@@ -15,48 +73,5 @@ module GildedRose
     when "Backstage passes to a TAFKAL80ETC concert"
       Backstage
     end
-  end
-end
-
-class Item
-  attr_reader :quality, :days_remaining
-  def initialize(quality, days_remaining)
-    @quality, @days_remaining = quality, days_remaining
-  end
-  def tick
-  end
-end
-
-class Normal < Item
-  def tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
-  end
-end
-
-class Brie < Item
-  def tick
-    @days_remaining -= 1
-    return if @quality >= 50
-
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
-  end
-end
-
-
-class Backstage < Item
-  def tick
-  
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
   end
 end
